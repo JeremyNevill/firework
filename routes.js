109fd2fd1180faf622fd33209ecd17482a2cbdbe
Jeremy Nevill <jeremy@nevill.net>
2014-12-16 23:48:56 +0000

    Added: Initial working rest api route GET

--- routes.js
+++ routes.js
@@ -123,20 +123,23 @@ if (Meteor.isClient) {
 
 }
 
+/*
+Rest ITEM Api
+*/
 Router.map(function() {
     this.route('methodExample', {
         path: '/api/items',
         where: 'server',
         action: function() {
-            // GET, POST, PUT, DELETE
+
             var requestMethod = this.request.method;
-            // Data from a POST request
-            var requestData = this.request.body;
-            // Could be, e.g. application/xml, etc.
             this.response.writeHead(200, {
                 'Content-Type': 'text/html'
             });
-            this.response.end('<html><body>Your request was a ' + requestMethod + '</body></html>');
+
+            this.response.end(JSON.stringify(
+                Items.find().fetch()
+            ));
         }
     });
 });
