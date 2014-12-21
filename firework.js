9eb88cce4f7e48bf361912e625157c6a81efd4ec
Jeremy Nevill <jeremy@nevill.net>
2014-12-21 22:48:20 +0000

    Added: Renames, moves and account

--- firework.js
+++ firework.js
@@ -139,12 +139,19 @@ if (Meteor.isClient) {
 
 
     /*
-     Stat Events
+     Account Events
      */
     Template.account.events({
+
         "change .hide-archived input": function(event) {
             Session.set("hideArchived", event.target.checked);
+        },
+
+        "submit .new-tokens": function(event) {
+            Meteor.call("createToken");
+            return false;
         }
+
     });
 
     /*
@@ -215,120 +222,3 @@ if (Meteor.isClient) {
     };
 
 }
-
-/*
- Meteor Methods
- */
-Meteor.methods({
-    addApiItem: function() {
-        // todo: Implement token based auth and post here
-
-    },
-    addItem: function(actor, action, amount, units, date) {
-        // Make sure the user is logged in before inserting
-        if (!Meteor.userId()) {
-            throw new Meteor.Error("not-authorized");
-        }
-
-        // Add actor if not existing already
-        var existingActor = Actors.findOne({
-            "actor": actor
-        });
-        if (typeof existingActor === 'undefined') {
-            Actors.insert({
-                actor: actor,
-                createdAt: new Date(),
-                owner: Meteor.userId(),
-                username: Meteor.user().username
-            });
-        }
-
-        // Add action if not existing already
-        var existingAction = Actions.findOne({
-            "action": action
-        });
-        if (typeof existingAction === 'undefined') {
-            Actions.insert({
-                action: action,
-                createdAt: new Date(),
-                owner: Meteor.userId(),
-                username: Meteor.user().username
-            });
-        }
-
-        // Add unit if not existing already
-        var existingUnit = Units.findOne({
-            "unit": units
-        });
-        if (typeof existingUnit === 'undefined') {
-            Units.insert({
-                unit: units,
-                createdAt: new Date(),
-                owner: Meteor.userId(),
-                username: Meteor.user().username
-            });
-        }
-
-        // Add the item
-        Items.insert({
-            actor: actor,
-            action: action,
-            amount: amount,
-            units: units,
-            date: date,
-            createdAt: new Date(),
-            owner: Meteor.userId(),
-            private: true,
-            username: Meteor.user().username
-        });
-    },
-    deleteItem: function(itemId) {
-        if (!Meteor.userId()) {
-            throw new Meteor.Error("not-authorized");
-        }
-
-        var item = Items.findOne(itemId);
-        if (item.private && item.owner !== Meteor.userId()) {
-            // If the task is private, make sure only the owner can delete it
-            throw new Meteor.Error("not-authorized");
-        }
-
-        Items.remove(itemId);
-    },
-
-    setChecked: function(itemId, setChecked) {
-        if (!Meteor.userId()) {
-            throw new Meteor.Error("not-authorized");
-        }
-
-        var item = Items.findOne(itemId);
-        if (item.private && item.owner !== Meteor.userId()) {
-            // If the task is private, make sure only the owner can delete it
-            throw new Meteor.Error("not-authorized");
-        }
-
-        Items.update(itemId, {
-            $set: {
-                checked: setChecked
-            }
-        });
-    },
-    setPrivate: function(itemId, setToPrivate) {
-        if (!Meteor.userId()) {
-            throw new Meteor.Error("not-authorized");
-        }
-
-        var item = Items.findOne(itemId);
-
-        // Make sure only the task owner can make a task private
-        if (item.owner !== Meteor.userId()) {
-            throw new Meteor.Error("not-authorized");
-        }
-
-        Items.update(itemId, {
-            $set: {
-                private: setToPrivate
-            }
-        });
-    }
-});
