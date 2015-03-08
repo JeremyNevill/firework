29624e45b66d05e260776b4b3cb29f33c8d5e53b
Jeremy Nevill <jeremy@nevill.net>
2015-03-08 22:00:13 +0000

    Added: Show only >0 count actor, actions and units

--- server/server.js
+++ server/server.js
@@ -10,40 +10,53 @@ if (Meteor.isServer) {
             sort: {
                 "createdAt": -1
             },
-            limit: 30
+            limit: 50,
+
         });
     });
 
     Meteor.publish("actors", function() {
         return Actors.find({
+            itemCount: {
+                $gt: 0
+            }
+        }, {
             owner: this.userId
         }, {
             sort: {
                 "actor": 1
             },
-            limit: 30
+            limit: 50
         });
     });
 
     Meteor.publish("actions", function() {
         return Actions.find({
+            itemCount: {
+                $gt: 0
+            }
+        }, {
             owner: this.userId
         }, {
             sort: {
                 "action": 1
             },
-            limit: 30
+            limit: 50
         });
     });
 
     Meteor.publish("units", function() {
         return Units.find({
+            itemCount: {
+                $gt: 0
+            }
+        }, {
             owner: this.userId
         }, {
             sort: {
                 "unit": 1
             },
-            limit: 30
+            limit: 50
         });
     });
 }
