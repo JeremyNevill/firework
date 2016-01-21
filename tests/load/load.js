dd27e87694e441bc6fb66819e0767b797696131e
Jeremy Nevill <jeremy@nevill.net>
2016-01-21 19:27:15 +0000

    Meteor update

--- tests/load/load.js
+++ tests/load/load.js
@@ -2,17 +2,17 @@ var request = require('request');
 
 var loopCounter = 0;
 var loopSize = 1000000;
-var loopInterval = 100; // ms
+var loopInterval = 50; // ms
 
 var i = setInterval(function() {
 
     request({
-        url: 'http://localhost:3000/api/items',
+        url: 'https://firework-jeremynevill-7.c9users.io/api/items',
         method: 'POST',
         headers: {
             'content-type': 'application/json',
-            'user-id': 'sxDqa3ZwXse84fj6Q',
-            'api-token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyaWQiOiJzeERxYTNad1hzZTg0Zmo2USJ9.K3LhkNRUh4phnXQoprdwB3bsYzx0DZT3WeGidMoymhY'
+            'user-id': 'ebbxy7Fxm9WmHQJjh',
+            'api-token': 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyaWQiOiJlYmJ4eTdGeG05V21IUUpqaCJ9.56LJbEJan5JWU5QfsLAWOJ9zRAt7_7PzY25nWoqR5M4'
         },
         json: {
             actor: 'Jimbo',
